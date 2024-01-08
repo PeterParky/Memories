22de31874c9a85ded6b3893eb3e46eb477687683
PHAM HOANG LONG <long23082000@gmail.com>
2024-01-09 01:15:28 +0900

    Update color for button

--- app/src/main/java/com/example/memories/MainActivity.java
+++ app/src/main/java/com/example/memories/MainActivity.java
@@ -61,21 +61,21 @@ public class MainActivity extends AppCompatActivity {
         np_year.setMinValue(1980);
         np_year.setMaxValue(year);
         np_year.setValue(year);
-        np_year.setTextColor(Color.BLACK);
+        SetColor(np_year,Color.BLACK);
         np_year.setTextSize(50); // Needs updating to be compatible with the device's screen size
 
         // set value for month
         np_month.setMinValue(1);
         np_month.setMaxValue(12);
         np_month.setValue(month);
-        np_month.setTextColor(Color.BLACK);
+        SetColor(np_month,Color.BLACK);
         np_month.setTextSize(50); // Needs updating to be compatible with the device's screen size
 
         // set value for day
         np_day.setMinValue(1);
         np_day.setMaxValue(31);
         np_day.setValue(day);
-        np_day.setTextColor(Color.BLACK);
+        SetColor(np_day,Color.BLACK);
         np_day.setTextSize(50); // Needs updating to be compatible with the device's screen size
 
         // Set up the listener to check and reset the value when the condition is not met
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // The function underlines selected word
-    public void Underline(Button button, String str_tmp) {
+    protected void Underline(Button button, String str_tmp) {
         try {
             SpannableString content = new SpannableString(str_tmp);
             content.setSpan(new UnderlineSpan(), 0, str_tmp.length(), 0);
@@ -133,101 +133,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-}
-// action to be performed when button Start is clicked
-       /* btn_birth.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // date picker dialog is used
-                // and its style and color are also passed
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, dateSetListener1, year, month, day);
-                // to set background for date-picker
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
-            }
-        });*/
-/*
+    // Set color for day-picker
+    protected void SetColor(NumberPicker np, int text_color) {
+        // Set color for text
+        np.setTextColor(text_color);
+        // Set color of the line separating the values
+        /*Still in the progress*/
 
-        // it is used to set the date which user selects
-        dateSetListener1 = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                // here month+1 is used so that
-                // actual month number can be displayed
-                // otherwise it starts from 0 and it shows
-                // 1 number less for every month
-                // example- for january month=0
-                month = month + 1;
-                String date = day + "/" + month + "/" + year;
-                btn_birth.setText(date);
-            }
-        };
-
-        // action to be performed when button 2 is clicked
-        btn_today.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // date picker dialog is used
-                // and its style and color are also passed
-                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, dateSetListener2, year, month, day);
-                // to set background for datepicker
-                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                datePickerDialog.show();
-            }
-        });
-*/
-
-       /* // it is used to set the date which user selects
-        dateSetListener2 = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                // here month+1 is used so that
-                // actual month number can be displayed
-                // otherwise it starts from 0 and it shows
-                // 1 number less for every month
-                // example- for january month=0
-                month = month + 1;
-                String date = day + "/" + month + "/" + year;
-                btn_today.setText(date);
-            }
-        };
-
-        // action to be performed when calculate button is clicked
-        btn_calculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // converting the inputted date to string
-                String sDate = btn_birth.getText().toString();
-                String eDate = btn_today.getText().toString();
-                SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd/MM/yyyy");
-                try {
-                    // converting it to date format
-                    Date date1 = simpleDateFormat1.parse(sDate);
-                    Date date2 = simpleDateFormat1.parse(eDate);
-
-                    long startdate = date1.getTime();
-                    long endDate = date2.getTime();
-
-                    // condition
-                    if (startdate <= endDate) {
-                        org.joda.time.Period period = new Period(startdate, endDate, PeriodType.yearMonthDay());
-                        int years = period.getYears();
-                        int months = period.getMonths();
-                        int days = period.getDays();
-
-                        // show the final output
-                        tvResult.setText(years + " Years |" + months + "Months |" + days + "Days");
-                    } else {
-                        // show message
-                        Toast.makeText(MainActivity.this, "BirthDate should not be larger than today's date!",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
-}*/
+
+}
